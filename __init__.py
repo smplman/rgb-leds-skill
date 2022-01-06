983918b3d03e385689553e465307b9fe19a9c648
smp4488 <smp4488@aol.com>
2022-01-06 14:39:40 -0500

    setting up intent

--- __init__.py
+++ __init__.py
@@ -14,30 +14,35 @@ LED_INVERT     = False   # True to invert the signal (when using NPN transistor 
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
 class RgbLeds(MycroftSkill):
-    def __init__(self):
-        MycroftSkill.__init__(self)
+	def __init__(self):
+		MycroftSkill.__init__(self)
+		self.strip = None
+		# # Create NeoPixel object with appropriate configuration.
+		# self.strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+		# # Intialize the library (must be called once before other functions).
+		# self.strip.begin()
 
-    def initialize(self):
-        #my_setting = self.settings.get('my_setting')
+	def initialize(self):
+		#my_setting = self.settings.get('my_setting')
+		# Create NeoPixel object with appropriate configuration.
+		self.strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+		# Intialize the library (must be called once before other functions).
+		self.strip.begin()
 
-        # Create NeoPixel object with appropriate configuration.
-        strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
-        # Intialize the library (must be called once before other functions).
-        strip.begin()
 
-    @intent_file_handler('leds.rgb.intent')
-    def handle_leds_rgb(self, message):
-        self.speak_dialog('leds.rgb')
+	@intent_file_handler('leds.rgb.intent')
+	def handle_leds_rgb(self, message):
+		self.speak_dialog('leds.rgb')
 
-    def colorSolid(strip, color, wait_ms=50):
-        """Wipe color across display a pixel at a time."""
-        for i in range(strip.numPixels()):
-            strip.setPixelColor(i, color)
-            #strip.show()
-            #time.sleep(wait_ms/1000.0)
+	def colorSolid(self, strip, color, wait_ms=50):
+		"""Wipe color across display a pixel at a time."""
+		for i in range(strip.numPixels()):
+			strip.setPixelColor(i, color)
+			#strip.show()
+			#time.sleep(wait_ms/1000.0)
 
-	strip.show
+		self.strip.show
 
 def create_skill():
-    return RgbLeds()
+	return RgbLeds()
 
